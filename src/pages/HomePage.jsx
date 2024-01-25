511c80b8cdcbb4df0fc0941ac6ae991c840b1d94
Oshioke-Salaki <salaki1902@gmail.com>
2024-01-25 17:28:52 +0100

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../ui/Navbar";
 import { useQuery } from "../context/QueryContext";
 import { useNavigate } from "react-router-dom";
+import { CovalentClient } from "@covalenthq/client-sdk";
 
 function HomePage() {
   const [chain, setChain] = useState("eth-mainnet");
   const [dex, setDex] = useState("uniswap_v2");
   const [poolAddress, setPoolAddress] = useState("");
+  const [samplePools, setSamplePools] = useState([]);
+  const [isLoadingPools, setIsLoadingPools] = useState(false);
   const { dispatch } = useQuery();
-
   const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   const fetchPools = async () => {
+  //     try {
+  //       setIsLoadingPools(true);
+  //       const client = new CovalentClient("cqt_rQkVVrThdHDvQ4MkG7br9J8BhrW4");
+  //       const resp = await client.XykService.getPools(chain, dex);
+  //       setSamplePools(resp);
+  //     } catch (err) {
+  //       console.log(err);
+  //     } finally {
+  //       setIsLoadingPools(false);
+  //     }
+  //   };
+
+  //   fetchPools();
+  //   console.log(samplePools);
+  // }, [chain, dex, samplePools]);
+
+  const dexs = [
+    {
+      chain: "eth-mainnet",
+      dexs: ["uniswap_v2", "sushiswap"],
+    },
+    {
+      chain: "matic-mainnet",
+      dexs: ["quickswap", "sushiswap", "apeswap_v2"],
+    },
+    {
+      chain: "bsc-mainnet",
+      dexs: [
+        "moonlift",
+        "sushiswap",
+        "apeswap_v2",
+        "pancakeswap_v2",
+        "empiredex",
+      ],
+    },
+    {
+      chain: "avalanche-mainnet",
+      dexs: ["pangolin", "sushiswap", "traderjoe"],
+    },
+  ];
 
   function handleSubmit() {
     dispatch({
@@ -42,10 +87,7 @@ function HomePage() {
                   className="text-black"
                   onChange={(e) => setChain(e.target.value)}
                 >
-                  <option value="eth-mainnet" selected>
-                    Ethereum
-                  </option>
-                  <option value="optimism-mainnet">Optimism</option>
+                  <option value="eth-mainnet">Ethereum</option>
                   <option value="matic-mainnet">Matic</option>
                   <option value="bsc-mainnet">BNB Smart Chain</option>
                   <option value="avalanche-mainnet">Avalanche</option>
@@ -56,13 +98,17 @@ function HomePage() {
                 <select
                   name="dex"
                   id="dex"
-                  value={dex}
+                  value={dexs.filter((curr) => curr.chain === chain)[0].dexs[0]}
                   className="text-black"
                   onChange={(e) => setDex(e.target.value)}
                 >
-                  <option value="uniswap_v2" selected>
-                    Uniswap
-                  </option>
+                  {dexs
+                    .filter((curr) => curr.chain === chain)[0]
+                    .dexs.map((dex, i) => (
+                      <option key={i} value={dex}>
+                        {dex}
+                      </option>
+                    ))}
                 </select>
               </div>
             </div>
@@ -84,6 +130,9 @@ function HomePage() {
                 onChange={(e) => setPoolAddress(e.target.value)}
                 className="mt-4 w-full rounded-lg border-[0.5px] border-solid border-[#c4c4c4] bg-white p-4 text-black outline-none"
               />
+            </div>
+            <div className="bg-white p-4">
+              {isLoadingPools ? "Loading sample pools" : <ul>loaded</ul>}
             </div>
 
             <button
